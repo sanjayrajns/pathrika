ef2e6ffaf2197e35a502fab441b86c8e1eef38a5
Sanjay Raj <sanjayrajns07@gmail.com>
2025-05-02 21:33:19 +0530

    Updated api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "85d357c47b9c41008bc36eb9a47c3b9a";
+const API_KEY = "YOUR_API_KEY";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("World"));
